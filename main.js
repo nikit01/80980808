d5a60e29dae28a120cc005561bfaa347a18e7ead
Nikita Osipov <112751679+nikit01@users.noreply.github.com>
2023-02-23 11:52:20 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+
+document.getElementById('menu').onclick = function() {
+    document.getElementById('text').classList.remove('hidden');
+}
+document.getElementById('media--btn--ex').onclick = function() {
+    document.getElementById('text').classList.add('hidden');
+}
